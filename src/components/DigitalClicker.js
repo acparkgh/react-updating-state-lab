b9b2a2b7c4215f1e7f4629cc5cbdd98ec1b7802f
acparkgh <acpark123@gmail.com>
2021-01-10 20:06:03 -0500

    completed

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,41 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+  
+  // handleClicked = () => {
+  //   let newTimesClicked = this.state.timesClicked
+  //   this.setState({
+  //     timesClicked: ++newTimesClicked
+  //   })
+  // }
+
+  handleClicked = () => {
+    this.setState( (previousState) => {
+      return ({
+        timesClicked: ++previousState.timesClicked
+      })
+    })
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClicked} >
+          {this.state.timesClicked}
+        </button>
+      </div>
+    )
+  }
+
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrate = () => {
+    this.setState({
+      settings: { ...this.state.settings, bitrate: 12 }
+    })
+  }
+
+  handleResolution = () => {
+    let settingsValue = this.state.settings
+    
+    this.setState({
+      settings: { ...settingsValue, video: { ...settingsValue.video, resolution: '720p' } }
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleBitrate} >
+          {this.state.settings.bitrate}
+        </button>
+        <button className="resolution" onClick={this.handleResolution} >
+          {this.state.settings.video.resolution}
+        </button>
+      </div>
+    )
+  }
+
+}
+
+export default YouTubeDebugger
